a8a03142ac1d9096707ae413e027caaec838365d
Mark Jubenville <markj@oanda.com>
2016-02-03 15:49:51 -0500

    added linting, some other updates

--- test/tests.js
+++ test/tests.js
@@ -4,15 +4,45 @@ var chai = require('chai');
 var log = require('../lib/log-defer');
 
 var expect = chai.expect;
-var should = chai.should();
 
-describe('log.error', function() {
-  it('should set the log level to 10', function() {
-    log.error({ message: 'this is a test' });
+describe('Log level helpers', function() {
+  describe('error', function() {
+    it('should set the log level to 10', function() {
+      log.error({ message: 'this is a test' });
 
-    var output = log.finalizeLog();
-    var outputObj = JSON.parse(output);
+      var outputObj = JSON.parse(log.finalizeLog());
 
-    expect(outputObj.logs[0][1]).to.equal(10);
+      expect(outputObj.logs[0][1]).to.equal(10);
+    });
+  });
+
+  describe('warn', function() {
+    it('should set the log level to 20', function() {
+      log.warn({ message: 'this is a test' });
+
+      var outputObj = JSON.parse(log.finalizeLog());
+
+      expect(outputObj.logs[0][1]).to.equal(20);
+    });
+  });
+
+  describe('info', function() {
+    it('should set the log level to 30', function() {
+      log.info({ message: 'this is a test' });
+
+      var outputObj = JSON.parse(log.finalizeLog());
+
+      expect(outputObj.logs[0][1]).to.equal(30);
+    });
+  });
+
+  describe('debug', function() {
+    it('should set the log level to 40', function() {
+      log.debug({ message: 'this is a test' });
+
+      var outputObj = JSON.parse(log.finalizeLog());
+
+      expect(outputObj.logs[0][1]).to.equal(40);
+    });
   });
 });
