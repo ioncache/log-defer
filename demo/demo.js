1d4161cb809b071b3a5645916f8ced6eb05c51be
Mark Jubenville <markj@oanda.com>
2016-03-01 18:20:11 -0500

    added data function

--- demo/demo.js
+++ demo/demo.js
@@ -9,6 +9,11 @@ log.warn('Warn test');
 log.error('Error test');
 log.debug('Debug test');
 log.timer('Helper Tests');
+
+log.warn('Warning with data!', { bar: 'baz', foo: 'bar' }, { barbaz: 'bazfoo', foobar: 'barbaz' });
+
+log.data({ a: 'b', b: 'c' });
+
 var output = log.finalizeLog();
 
 process.stdout.write(output);
