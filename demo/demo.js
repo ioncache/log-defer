8c99e7a1498c7bbdbf11786327aa0ba61dab60dc
Mark Jubenville <markj@oanda.com>
2016-03-03 15:00:23 -0500

    updated demo and tests

--- demo/demo.js
+++ demo/demo.js
@@ -1,25 +1,53 @@
 'use strict';
 
+// npm modules
 var log = require('../lib/log-defer');
 var process = require('process');
-var sleep = require('sleep');
+var q = require('q');
 
-log.timer('Log Messages'); // begin a timer called 'Log Messages'
+var deferreds = [];
+
+log.timer('Log Messaging Demo'); // begin a timer called 'Log Messaging Demo'
+
 log.info('Info level logging');
 log.warn('Warn level logging');
 log.error('Error level logging');
 log.timer('Interleaved, longer running timer');
 log.debug('Debug level logging');
-log.timer('Short timer'); // start
-sleep.sleep(1);
-log.timer('Short timer'); // end
-sleep.sleep(2);
-log.timer('Log Messages'); // end the 'Log Messages' timer
+
+log.timer('async timer');
+log.info('starting async timer');
+var deferred1 = q.defer();
+deferreds.push(deferred1.promise);
+setTimeout(function() {
+  log.info('finishing async timer');
+  log.timer('async timer');
+  deferred1.resolve();
+}, 375);
+
+log.timer('Short timer');
+var deferred2 = q.defer();
+deferreds.push(deferred2);
+setTimeout(function() {
+  log.timer('Short timer');
+  deferred2.resolve();
+}, 125);
+
 log.data({ bar: 'baz', foo: 'bar' }); // add to the data object in the log-defer
 log.warn('Warning with data!', { bar: 'baz', foo: 'bar' }, { barbaz: 'bazfoo', foobar: 'barbaz' });
-sleep.sleep(3);
-log.timer('Interleaved, longer running timer');
 
-var output = log.finalizeLog(); // finalize the log, and return a json string of the log-defer
+var deferred3 = q.defer();
+deferreds.push(deferred3.promise);
+setTimeout(function() {
+  log.timer('Interleaved, longer running timer');
+  deferred3.resolve();
+}, 700);
 
-process.stdout.write(output);
+log.data({ quux: 'xyzzy', qux: 'corge' });
+
+q.all(deferreds).then(function() {
+  log.timer('Log Messaging Demo'); // end the 'Log Messaging Demo' timer
+  var output = log.finalizeLog(); // finalize the log, and return a json string of the log-defer
+
+  process.stdout.write(output);
+});
--- test/tests.js
+++ test/tests.js
@@ -3,6 +3,7 @@
 var _ = require('lodash');
 var chai = require('chai');
 var log = require('../lib/log-defer');
+var q = require('q');
 
 var expect = chai.expect;
 
@@ -149,6 +150,61 @@ describe('Log level helpers', function() {
 
       // Assert
       expect(outputObj.logs[0][1]).to.equal(40);
+    });
+  });
+});
+
+describe('multiple logs, timers, and data', function() {
+  it('the output should match all log statements', function() {
+    // Act
+    var deferreds = [];
+
+    log.timer('Log Messaging Demo'); // begin a timer called 'Log Messaging Demo'
+
+    log.info('Info level logging');
+    log.warn('Warn level logging');
+    log.error('Error level logging');
+    log.timer('Interleaved, longer running timer');
+    log.debug('Debug level logging');
+
+    log.timer('async timer');
+    log.info('starting async timer');
+    var deferred1 = q.defer();
+    deferreds.push(deferred1.promise);
+    setTimeout(function() {
+      log.info('finishing async timer');
+      log.timer('async timer');
+      deferred1.resolve();
+    }, 375);
+
+    log.timer('Short timer');
+    var deferred2 = q.defer();
+    deferreds.push(deferred2);
+    setTimeout(function() {
+      log.timer('Short timer');
+      deferred2.resolve();
+    }, 125);
+
+    log.data({ bar: 'baz', foo: 'bar' }); // add to the data object in the log-defer
+    log.warn('Warning with data!', { bar: 'baz', foo: 'bar' }, { barbaz: 'bazfoo', foobar: 'barbaz' });
+
+    var deferred3 = q.defer();
+    deferreds.push(deferred3.promise);
+    setTimeout(function() {
+      log.timer('Interleaved, longer running timer');
+      deferred3.resolve();
+    }, 700);
+
+    log.data({ quux: 'xyzzy', qux: 'corge' });
+
+    // Assert
+    q.all(deferreds).then(function() {
+      log.timer('Log Messaging Demo'); // end the 'Log Messaging Demo' timer
+
+      var output = log.finalizeLog(); // finalize the log, and return a json string of the log-defer
+      var outputObj = JSON.parse(output);
+
+      expect(outputObj).to.be.defined;
     });
   });
 });
